3a15f00b3ae0e2c409091b8dcffabbcf2e963231
yusei00nagai <yusei.nagai1013@gmail.com>
2023-08-01 19:28:19 +0900

    create main詰み　とりあえずコミット

--- frontend/src/modules/all/Mainvisual.tsx
+++ frontend/src/modules/all/Mainvisual.tsx
@@ -1,10 +1,17 @@
 import { Box, SxProps, Theme } from "@mui/material";
+import { MainContent } from "../create/MainContent";    //createの関数
+
+
+// { MainContent };      //こんな感じで使えると思ったけど何もなにも表示されず、、、
 
 export const Mainvisual = () => {
   return (
     <Box className="Mainvisual" sx={sx}>
-      メインヴィジュアル
+      <Box>
+        [MainContent]  {/* こんな感じで使えると思ったけど何もなにも表示されず、、、 */}
+      </Box>
     </Box>
+    
   );
 };
 
